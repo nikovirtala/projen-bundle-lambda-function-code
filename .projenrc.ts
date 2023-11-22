d52791d19314a201f5f016ab60c9cb34a7e52a4d
Niko Virtala <niko.virtala@hey.com>
2023-11-22 16:58:43 +0200

    chore: change npm access level

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,12 @@
 import { typescript } from "projen";
+import { NpmAccess } from "projen/lib/javascript";
 const project = new typescript.TypeScriptProject({
   copyrightOwner: "Niko Virtala",
   defaultReleaseBranch: "main",
   deps: ["projen"],
   license: "MIT",
   name: "projen-bundle-lambda-function-code",
+  npmAccess: NpmAccess.PUBLIC,
   prettier: true,
   projenrcTs: true,
 });
