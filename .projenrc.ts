2399cb66da5bc7b625e5f0c39b3bca1d4481aaf5
Niko Virtala <niko.virtala@hey.com>
2023-11-22 16:50:24 +0200

    chore: add licence

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,9 @@
 import { typescript } from "projen";
 const project = new typescript.TypeScriptProject({
+  copyrightOwner: "Niko Virtala",
   defaultReleaseBranch: "main",
   deps: ["projen"],
+  license: "MIT",
   name: "projen-bundle-lambda-function-code",
   prettier: true,
   projenrcTs: true,
