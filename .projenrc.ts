486d979d35ae04862341c89a67af781dec6711f9
Niko Virtala <niko.virtala@hey.com>
2023-11-22 17:09:29 +0200

    chore: disable jest

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,6 +3,7 @@ const project = new typescript.TypeScriptProject({
   copyrightOwner: "Niko Virtala",
   defaultReleaseBranch: "main",
   deps: ["projen"],
+  jest: false,
   license: "MIT",
   name: "projen-bundle-lambda-function-code",
   prettier: true,
