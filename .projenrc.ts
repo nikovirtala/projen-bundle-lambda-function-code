89ef9e82fbcf48a2933f5a88fa916ac2c9a4b7bf
Niko Virtala <niko.virtala@hey.com>
2023-11-22 16:50:24 +0200

    chore: release to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,15 @@
 import { typescript } from "projen";
+import { NpmAccess } from "projen/lib/javascript";
 const project = new typescript.TypeScriptProject({
+  copyrightOwner: "Niko Virtala",
   defaultReleaseBranch: "main",
   deps: ["projen"],
-  name: "projen-bundle-lambda-function-code",
+  jest: false,
+  license: "MIT",
+  name: "@nikovirtala/projen-bundle-lambda-function-code",
+  npmAccess: NpmAccess.PUBLIC,
   prettier: true,
   projenrcTs: true,
+  releaseToNpm: true,
 });
 project.synth();
