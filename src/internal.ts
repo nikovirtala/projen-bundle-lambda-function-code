56d8c44be7f37526051eacc02db41fa5d78bc599
Niko Virtala <niko.virtala@hey.com>
2023-11-23 11:54:45 +0200

    feat: configurable function name extension

--- src/internal.ts
+++ src/internal.ts
@@ -1,11 +1,6 @@
 import { sep, posix } from "path";
 
 /**
- * Suffix for AWS Lambda handlers.
- */
-export const TYPESCRIPT_LAMBDA_EXT = ".lambda.ts";
-
-/**
  * Converts the given path string to posix if it wasn't already.
  */
 export function convertToPosixPath(p: string) {
