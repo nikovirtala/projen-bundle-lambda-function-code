ce634093d0ebde95d01d4acb1cf982d1b3c7e942
Niko Virtala <niko.virtala@hey.com>
2023-11-24 17:00:42 +0200

    chore(deps): add cdk-esbuild

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,11 +2,12 @@ import { typescript, javascript } from "projen";
 const project = new typescript.TypeScriptProject({
   copyrightOwner: "Niko Virtala",
   defaultReleaseBranch: "main",
-  deps: ["case", "projen"],
+  deps: ["case", "projen", "@mrgrain/cdk-esbuild"],
   jest: false,
   license: "MIT",
   name: "@nikovirtala/projen-bundle-lambda-function-code",
   npmAccess: javascript.NpmAccess.PUBLIC,
+  peerDeps: ["aws-cdk-lib@^2.51.0"],
   prettier: true,
   projenrcTs: true,
   releaseToNpm: true,
